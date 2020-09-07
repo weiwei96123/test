ccc3107f71a1d8cff91ee8c514a7d03945e5268e
weiwei96123 <70866867+weiwei96123@users.noreply.github.com>
2020-09-07 19:02:14 +0800

    Update lunjian.java

--- src/main/java/com/atguigu/git/test/lunjian.java
+++ src/main/java/com/atguigu/git/test/lunjian.java
@@ -2,7 +2,7 @@ package com.atguigu.git.test;
 
 public class lunjian {
     public static void main(String[] args) {
-        System.out.println("剑法厉害");
+        System.out.println("千秋万代");
         System.out.println("武林称霸");
         System.out.println("一统江湖");
     }
